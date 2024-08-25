52a5e9c23e58032fe9f268e3db72ef8ec97d7c62
seungwoo1209 <2002msw@naver.com>
2024-08-25 11:16:03 +0900

    new post

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1724454894',resources: [
+    cacheName: 'chirpy-1724552134',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
